614bd190ea599de9f4f972f9ddf6e78e20a97e5a
saurav95 <passiontowork222@gmail.com>
2016-07-28 05:04:03 -0700

    check social links

--- js/custom.js
+++ js/custom.js
@@ -15,25 +15,59 @@ $('.grid').masonry({
   percentPosition: true,
 });
 
-$('.flexslider').flexslider({
-animation: "slide",
-controlNav: false,
-directionNav: true,
-slideshowSpeed: 5000,
-animationSpeed: 600,
-touch: true,
-start: function(slider) {
-            $('#sliderNext').on('click', function(e){
-                $('.flex-next').trigger('click');
-            });
-            $('#sliderPrev').on('click', function(e){
-                $('.flex-prev').trigger('click');
-            });	
-            $('.total-slides').text(slider.count);
-          },
-          after: function(slider) {
-            $('.current-slide').text(slider.currentSlide+1);
-         }
+if($('.flexslider').length){
+	$('.flexslider').flexslider({
+	animation: "slide",
+	controlNav: false,
+	directionNav: true,
+	slideshowSpeed: 3000,
+	animationSpeed: 600,
+	touch: true,
+	start: function(slider) {
+				$('#sliderNext').on('click', function(e){
+					$('.flex-next').trigger('click');
+				});
+				$('#sliderPrev').on('click', function(e){
+					$('.flex-prev').trigger('click');
+				});	
+				$('.total-slides').text(slider.count);
+			  },
+			  after: function(slider) {
+				$('.current-slide').text(slider.currentSlide+1);
+			 }
+	});
+}
+
 });
 
+$(document).ready(function(){
+   $('li[data-related = "info"]').click(function(){
+	   $('div#share').removeClass('active');
+	   $('div#info').toggleClass('active');
+	   $('li[data-related = "share"] .close').hide();
+	   $(this).find('.close').toggle();
+		if ( $('div.pop_up').hasClass('active') )
+		{
+			   $('.pop_up_wrapper').addClass('open');
+		}
+		else{
+			$('.pop_up_wrapper').removeClass('open');
+		}		
+   });
+	
+   $('li[data-related = "share"]').click(function(){
+	   $('div#info').removeClass('active');   
+	   $('div#share').toggleClass('active');
+	   $('li[data-related = "info"] .close').hide();
+	   $(this).find('.close').toggle();
+		
+		if ( $('div.pop_up').hasClass('active') )
+		{
+			   $('.pop_up_wrapper').addClass('open');
+		}
+		else{
+			$('.pop_up_wrapper').removeClass('open');
+		}
+	   
+   });   
 });
